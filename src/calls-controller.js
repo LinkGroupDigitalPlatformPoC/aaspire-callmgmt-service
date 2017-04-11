ed9c5a3a057e025aed1188c7e97049533235d617
MANGALAGANESH BALASUBRAMANIAN <bmangalaganesh@au1.ibm.com>
2017-04-11 11:51:20 +1000

    support the various attributes for a call. Some fields are marked as mandatory.

--- src/calls-controller.js
+++ src/calls-controller.js
@@ -19,10 +19,49 @@ class CallsController {
         });
     }
 
+    //Here is the sample body of a well-defined call 
+    /*
+    {   
+    	      "notes":{  
+    	         "type":"string",
+    	         "description":"Notes captured during engagement",
+    	         "example":"Member expressed frustration at premium"
+    	      },
+    	      "status":{  
+    	         "type":"string",
+    	         "description":"Current status of the engagement (In Progress|Closing|Completed|Disconnected)",
+    	         "example":"In Progress"
+    	      },
+    	      "dateTimeCompleted":{  
+    	         "type":"string",
+    	         "description":"Date and time the engagement was completed",
+    	         "example":"2017-07-31 09:35"
+    	      },
+    	      "dateTimeInitiated":{  
+    	         "type":"string",
+    	         "description":"Date the calls as initiated",
+    	         "example":"2017-07-31 09:30"
+    	      },
+    	      "engagementType":{  
+    	         "type":"string",
+    	         "description":"The type of engagement (Call Centre|In Person|Video Call|Chat)",
+    	         "example":"Call Centre"
+    	      },
+    	      "memberId":{  
+    	         "type":"string",
+    	         "description":"Member identifier",
+    	         "example":"23544334346"
+    	      }
+    	   }
+   */
+    
     createCall(request, response) {
         invariant(!util.isNullOrUndefined(request.body.memberId), "Must have memberId");
+        invariant(!util.isNullOrUndefined(request.body.dateTimeInitiated), "Must have Date Time Initiated");
+        invariant(!util.isNullOrUndefined(request.body.status), "Must have status");
+        
 
-        const newCall = { memberId: request.body.memberId };
+        const newCall = { memberId: request.body.memberId, dateTimeInitiated: request.body.dateTimeInitiated ,  dateTimeCompleted: request.body.dateTimeCompleted , notes: request.body.notes , primaryTopic: request.body.primaryTopic, secondaryTopic: request.body.secondaryTopic };
         this.mongodb.collection("calls").insertOne(newCall, function(error, result) {
             if (error) {
                 response.status(500).send(error);
