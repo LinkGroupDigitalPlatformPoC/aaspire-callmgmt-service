c1ac84c26d23f5d90785a9c1850609659274a8a2
MANGALAGANESH BALASUBRAMANIAN <bmangalaganesh@au1.ibm.com>
2017-04-11 14:31:01 +1000

    refactoring changes

---
+++ app/src/call.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+const util = require('util');
+const invariant = require('invariant');
+
+class Call {
+
+	constructor(body) {
+		invariant(!util.isNullOrUndefined(body.memberId), "Must have memberId");
+		invariant(!util.isNullOrUndefined(body.dateTimeInitiated), "Must have Date Time Initiated");
+		invariant(!util.isNullOrUndefined(body.status), "Must have status");
+	        
+    	this.memberId = body.memberId;
+    	this.dateTimeInitiated = body.dateTimeInitiated;
+    	this.dateTimeCompleted = body.dateTimeCompleted ;
+    	this.notes = body.notes;
+    	this.primaryTopic = body.primaryTopic;
+    	this.secondaryTopic = body.secondaryTopic ;
+    	this.status = body.status ;
+		
+	}
+}
+
+module.exports=Call;
